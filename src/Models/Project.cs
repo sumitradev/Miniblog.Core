2c601f666c857bb8687b6811e2271a12e9a977e1
sumitradev <baruasumitradev@gmail.com>
2026-02-15 10:32:16 +1000

    comments on project.cs

--- src/Models/Project.cs
+++ src/Models/Project.cs
@@ -2,6 +2,7 @@
 
 public class Project
 {
+    // project has got more fields , for the time being we will work on few fields
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
